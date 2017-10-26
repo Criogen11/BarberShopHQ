32d31724dc924dcd19a0e7f7d293e7310a7eb2b8
Criogen11 <tsv1985@bk.ru>
2017-10-26 21:05:30 +0300

    Add validation

--- app.rb
+++ app.rb
@@ -9,7 +9,10 @@ set :database, "sqlite3:barbershop.db"
 
 # Создаем сущьность
 class Client < ActiveRecord::Base
-
+	validates :name, presence: true
+	validates :phone, presence: true
+	validates :datestamp, presence: true
+	validates :barber, presence: true
 end
 
 class Barber < ActiveRecord::Base
